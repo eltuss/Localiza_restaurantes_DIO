e44ee984eb4a54cad8a65b247d40bc06a1c5e4de
Eltuss <elton.s.barbosa@outlook.com>
2021-09-25 11:23:00 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Home from './pages/Home';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+import { Reset } from 'styled-reset';
+
+const App =() => {
+  return(
+    <ThemeProvider theme={theme}>
+      <Reset/>
+      <Home/>
+    </ThemeProvider>
+  
+  );
+}
+export default App;
