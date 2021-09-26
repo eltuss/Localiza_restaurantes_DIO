68c14414e586fe5b7a1454776e7c29b96b50ea94
Eltuss <elton.s.barbosa@outlook.com>
2021-09-26 12:30:06 -0300

    Modal criado

---
+++ src/components/RestaurantCard/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+
+import restaurante from "../../assets/restaurante-fake.png";
+
+
+import { Restaurant, RestaurantInfo, RestaurantPhoto, Title, Address } from './styles';
+
+const RestaurantCard = () => (
+<Restaurant>
+    <RestaurantInfo>
+        <Title>Nome do restaurante</Title>
+        <ReactStars count={5} isHalf value={4} edit={false} activeColor="#e7711c" />
+        <Address>Endereço</Address>
+    </RestaurantInfo>
+    <RestaurantPhoto src={restaurante}/>
+</Restaurant>
+);
+
+export default RestaurantCard;
+
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,63 @@
 
-import React from "react";
-import { Container } from "./styles";
+import React, { useState } from "react";
+import logo from '../../assets/localiza_restaurante_logo.png'
+import restaurante from '../../assets/restaurante-fake.png'
+import TextField, { Input } from "@material/react-text-field/dist/index";
+import MaterialIcon from '@material/react-material-icon';
 
-const Home = () => <Container>Hello World</Container>;
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle, Carousel } from "./styles";
+import {Card, RestaurantCard, Modal} from '../../components';
+
+const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [modalOpened, setModalOpened] = useState(false);
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+      };
+
+return(
+    <Wrapper>
+<Container>
+    
+    <Search>
+        <Logo src={logo} alt="Logo do restaurante" />
+        <TextField
+        label="Pesquisar"
+        outlined
+        //onTrailingIconSelect={() => this.setState({ value: '' })}
+        trailingIcon={<MaterialIcon role="button" icon="search" />}>
+
+        <Input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        />
+        </TextField>
+        <CarouselTitle>Na sua Área</CarouselTitle>
+        <Carousel {...settings}>
+            <Card photo={restaurante} title="Nome do Restaurante 01"/>
+            <Card photo={restaurante} title="Nome do Restaurante 02"/>
+            <Card photo={restaurante} title="Nome do Restaurante 03"/>
+            <Card photo={restaurante} title="Nome do Restaurante 04"/>
+            <Card photo={restaurante} title="Nome do Restaurante 05"/>
+            <Card photo={restaurante} title="Nome do Restaurante 06"/>
+            <Card photo={restaurante} title="Nome do Restaurante 07"/>
+            
+        </Carousel>
+        <button onClick={() => setModalOpened(true)}>Abrir Modal</button>
+    </Search>
+    <RestaurantCard/>
+    </Container>
+    <Map/>
+    <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} />
+    </Wrapper>
+    
+);
+}
 
 export default Home;
